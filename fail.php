b89a65685c939d14fddf2f98bf9fd8bce3a7b1af
Maxence <maxence.bohard@gmail.com>
2019-09-06 11:52:03 +0200

    connexion

---
+++ fail.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+
+<html>
+<head>
+
+<link rel="stylesheet" href="css/bootstrap-min.css">
+<link rel="stylesheet" href="css/cssprincipal.css">
+
+</head>
+
+<body>
+
+<div class="messagealert">
+<div class="alert alert-dismissible alert-warning">
+  <h4 class="alert-heading">Attention </h4>
+  <p class="mb-0">Mauvais identifiant ou mot de passe ! <a href="pagedelog.php" class="alert-link"><u>Veuillez réessayer</u></a>.</p>
+</div>
+</div>
+
+
+</body>
+
+
+
+</html>
