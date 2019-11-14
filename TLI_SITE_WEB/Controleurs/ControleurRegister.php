48daa75bdef35655c51f814bc808cbd0a4cf663c
RobinDeville <47632795+RobinDeville@users.noreply.github.com>
2019-11-14 17:04:00 +0100

    Update ControleurRegister.php

--- TLI_SITE_WEB/Controleurs/ControleurRegister.php
+++ TLI_SITE_WEB/Controleurs/ControleurRegister.php
@@ -5,6 +5,7 @@ class ControleurRegister
 {
   public static Function register()
   {
+	  // Set des variables php avec les éléments reçus du formulaire html
     $Nom=$_POST['nom'];
     $Prenom=$_POST['prenom'];
     $Mail = $_POST['email'];
@@ -12,12 +13,16 @@ class ControleurRegister
     $Confirm_password=$_POST['confirm_password'];
 	  if($Password==$Confirm_password)
       {
+		  // hashage du password
         $Password=password_hash($Password, PASSWORD_BCRYPT);
+		  
+		  // Création de l'utilisateur dans la bdd
         $User = new User($Mail,$Password,$Nom,$Prenom);
 			  $User->AddUser();
       }
     else
       {
+	    // Resultat si les mdp renseignés dans le formulaire ne sont pas identiques
 			return "mots de passe pas identiques";
 			//echo 'mot de passe pas identique';
 			 //$smarty->assign("ErrorMessage","Les deux mots de passe ne sont identiques");
