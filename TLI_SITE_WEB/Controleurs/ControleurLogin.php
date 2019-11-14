823e34b882a7ca868ba47117e29b20b5caf538c4
RobinDeville <47632795+RobinDeville@users.noreply.github.com>
2019-11-14 17:01:25 +0100

    Update ControleurLogin.php

--- TLI_SITE_WEB/Controleurs/ControleurLogin.php
+++ TLI_SITE_WEB/Controleurs/ControleurLogin.php
@@ -4,10 +4,17 @@ class ControleurLogin
 {
     public static Function login()
     {
+        // Set des variables php avec les informations reçue venant du formulaire html
       $mail=$_POST['login'];
       $password=$_POST['password'];
+        
+        // Lancement de la fonction modele login 
       $user=User::login($mail,$password);
+        
+        //Création de la session avec le résultat de la fonction modele
       $_SESSION['user']=$user;
+        
+        // Si la session est initialisée, on appel l'index. Sinon, mauvais mdp.
       if(isset($_SESSION['user']))
       {
         header("Location:index.php");        
@@ -17,6 +24,8 @@ class ControleurLogin
         return "mauvais mdp";
       }
     }
+    
+    // Deconnexion (suppression session)
     public static Function logout()
       {
   			session_unset();
